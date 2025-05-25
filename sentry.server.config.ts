6b74bfaae7e96e5a7fc784d8eb94aa518c2cf2c6
Kandy-Hamisi <hkandy2000@gmail.com>
2025-05-25 21:44:04 +0300

    neon db setup and sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://026132e290a4b70c7e4cdc4d43cf775a@o4509355368841216.ingest.de.sentry.io/4509355420090448",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
